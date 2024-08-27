d1a27ac1dc9cbbff4e9e8f04737949f94bddf840
Angel-2180 <a.gimer@student.isartdigital.com>
2024-08-27 04:14:28 -0400

    refactor: Remove commented out code in main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,19 +16,19 @@ use crate::cmd::{Cmd, Run};
 use crate::error::SilentExit;
 
 // -- main code --
-// fn main() -> ExitCode {
-//     match Cmd::parse().run() {
-//         Ok(()) => ExitCode::SUCCESS,
-//         Err(e) => match e.downcast::<SilentExit>() {
-//             Ok(SilentExit { code }) => code.into(),
-//             Err(e) => {
-//                 _ = writeln!(io::stderr(), "MediaSort: {e:?}");
-//                 return ExitCode::FAILURE;
-//             }
-//         },
-//     };
-//     ExitCode::SUCCESS
-// }
+fn main() -> ExitCode {
+    match Cmd::parse().run() {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(e) => match e.downcast::<SilentExit>() {
+            Ok(SilentExit { code }) => code.into(),
+            Err(e) => {
+                _ = writeln!(io::stderr(), "MediaSort: {e:?}");
+                return ExitCode::FAILURE;
+            }
+        },
+    };
+    ExitCode::SUCCESS
+}
 
 
 
@@ -37,28 +37,34 @@ use crate::error::SilentExit;
 
 
 // -- test code --
-fn main() -> Result<(), Error> {
-    //     let series = "Mushoku Tensei: Jobless Reincarnation";
-    //     let year = "2021";
-    //     let media_type = search::result::SERIES.clone();
-    //     let results = search::search_tvmaze::search_tvmaze(series, year, media_type).unwrap();
-    //     for result in results {
-    //         println!("{}", result.string());
-    //     }
-    let query = "Interstellar";
-    let year = Some("");
-    let media_type = search::result::MediaType::Movie;
+// fn main() -> Result<(), Error> {
+//     let series = "Mushoku Tensei: Jobless Reincarnation";
+//     let year = "2021";
+//     let media_type = search::result::SERIES.clone();
+//     let results = search::search_tvmaze::search_tvmaze(series, year, media_type).unwrap();
+//     for result in results {
+//         println!("{}", result.string());
+//     }
+//     let query = "Bocchi the Rock";
+//     let year = Some("");
+//     let media_type = search::result::MediaType::Series;
 
-    let debug_mode = true;
+//     let debug_mode = true;
+//     let moviedb_results = search::search_tmdb::search_movie_db(query, year, media_type, debug_mode)?;
+//     let mut found: bool = false;
+//     for result in moviedb_results {
+//         if result.accuracy < 95 {
+//             continue;
+//         }
+//         println!("{}, accuracy = {}", result.string(), result.accuracy);
+//         found = true;
+//     }
+//     if !found {
+//         let tv_maze_results = search::search_tvmaze::search_tvmaze(query, year, media_type)?;
+//         for result in tv_maze_results {
+//             println!("{}, accuracy = {}", result.string(), result.accuracy);
+//         }
+//     }
 
-    let results = search::search_tmdb::search_movie_db(query, year, media_type, debug_mode)?;
-
-    for result in results {
-        if result.accuracy < 95 {
-            continue;
-        }
-        println!("{}, accuracy = {}", result.string(), result.accuracy);
-    }
-
-    Ok(())
-}
+//     Ok(())
+// }
